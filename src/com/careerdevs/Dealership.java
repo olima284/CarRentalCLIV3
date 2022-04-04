1e6213fbf8b2c7756e501e2f0f7be6f758448746
olima284 <olivialima.student@careerdevs.com>
2022-04-04 12:19:10 -0400

    done

--- src/com/careerdevs/Dealership.java
+++ src/com/careerdevs/Dealership.java
@@ -52,9 +52,24 @@ public class Dealership {
 
     }
     public void buyCars(String license){
+            if(availableCars.size() == 0 ){
+                System.out.println("We are out of Cars");
+                return;
+
+            }
+             for (int i = 0; i < availableCars.size(); i++) {
+                 if (license.equals(availableCars.get(i).getPlateNum())) {
+                  soldCars.add(availableCars.get(i));
+                  availableCars.remove(availableCars.get(i));
+                  return;
+                 }
+             }
 
     }
 }
+
+
+
 
 
 
@@ -63,4 +78,8 @@ public class Dealership {
 //    List<Car> availableCars;
 //    List<Car> soldCars
 // String as long as it acts as a unique identifier for your car.
+//method called “buyCar(String license)”.
+// method will take in a license plate number,
+// and will loop through the availableCars inventory.
+// If the license plate passed into the method matches a license plate of a car in our ArrayList, then we will place it in the soldCars array, and remove it from the availableCars array
 //*/
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,19 +5,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
+
+
 
 
 
         Cars cars1 = new Cars("Honda", "Accord", (short) 2010, "AW246");
         Cars cars2 = new Cars("Chevy", "Cruze", (short) 2020, "MV235");
         Cars cars3 = new Cars("Toyota", "Corolla", (short) 2011, "TC784");
-        Dealership dealership1 = new Dealership("Liv's Auto Sales");
+        Dealership Dealership1 = new Dealership("Liv's Auto Sales");
 
-        dealership1.getAvailableCars().add(cars1);
-        dealership1.getAvailableCars().add(cars2);
-        dealership1.getAvailableCars().add(cars3);
+        Dealership1.getAvailableCars().add(cars1);
+        Dealership1.getAvailableCars().add(cars2);
+        Dealership1.getAvailableCars().add(cars3);
 
+        System.out.println("Welcome" + " "+ Dealership1.getDealershipName());
+
+
+        for (int i = 0; i < Dealership1.getAvailableCars().size(); i++) {
+            System.out.println(Dealership1.getAvailableCars().get(i));
+        }
+
+        System.out.println("Please enter Plate Number of desired car : " );
+            String userInput = scanner.nextLine();
+
+            Dealership1.buyCars(userInput);
+
+            System.out.println("You have Purchased: " + Dealership1.getSoldCars());
+
+            System.out.println("Remaining Cars for sales: \n " + Dealership1.getAvailableCars());
        // test dealership name
       //System.out.println("Welcome" + " "+ dealership1.getDealershipName());
        // print:Liv's Auto Sales
